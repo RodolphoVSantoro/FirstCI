da69ed27b10f5a88aacdc752911472dd062c7766
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-04-08 21:52:48 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+
+def soma(a,b):
+    """Return a friendly HTTP greeting."""
+    return a+b
+
+if __name__ == '__main__':
+    print(soma(4,5))
---
+++ test.py
@@ -0,0 +1,4 @@
+from main import *
+
+def test_soma():
+   assert soma(5,7) == 12
