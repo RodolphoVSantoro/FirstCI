22af9beecf3e891af5828b9704b80f08489dc4b1
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-04-09 22:49:00 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ import numpy as np
 
 def soma(a,b):
     """Return a friendly HTTP greeting."""
-    return a+b+1
+    return a+b
+
+def subtracao(a,b):
+    return a-b
 
 if __name__ == '__main__':
     print(soma(4,5))
